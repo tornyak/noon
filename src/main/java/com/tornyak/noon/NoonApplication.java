89dbe34845f82305ff16ee8bd51333fe97162075
Vanja <vanja.divkovic@gmail.com>
2017-09-29 01:12:55 +0200

    Project created

---
+++ src/main/java/com/tornyak/noon/NoonApplication.java
@@ -0,0 +1,12 @@
+package com.tornyak.noon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoonApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tornyak/noon/NoonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tornyak.noon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NoonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
